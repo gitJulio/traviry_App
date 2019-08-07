45a563113e474ab77e13f22950240eaf9c97e00d
Julio Bueso <julio.ezequiel@gmail.com>
2019-08-07 14:19:52 -0600

    traviry login o register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'login', loadChildren: './paginas/login/login.module#LoginPageModule' },
+  { path: 'registro', loadChildren: './paginas/registro/registro.module#RegistroPageModule' },
 ];
 
 @NgModule({
