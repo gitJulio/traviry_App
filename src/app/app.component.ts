4e4e6c4023b1f5d39317579a378e3d7e3c38f82b
Julio Bueso <julio.ezequiel@gmail.com>
2019-09-17 12:17:08 -0600

    mejoras perfil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,30 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+  es_login:any=false;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private ruta:Router
   ) {
     this.initializeApp();
+  }
+
+  ngOnInit() {
+    this.ruta.events.subscribe(
+     async (event: any) => {
+       if (event instanceof NavigationEnd) {
+           if (this.ruta.url == '/home' || this.ruta.url == '/login' || this.ruta.url == '/registro') {
+             this.es_login = true;
+           } else {
+             this.es_login = false;
+           }
+         }
+     }
+ );
+ 
   }
 
   initializeApp() {
--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,14 +8,41 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
+  muro:boolean=false;
+  add:boolean=false;
+  perfil:boolean=false;
+  chat:boolean=false;
+  buscar:boolean=false;
+  notificaciones:boolean=false;
   constructor(
           public ruta:Router
   ) { }
 
   ngOnInit() {
 
-   console.log(this.ruta.url );
-   
+   this.ruta.events.subscribe(
+    async (event: any) => {
+      if (event instanceof NavigationEnd) {
+                if (this.ruta.url == '/perfil') {
+                  this.perfil=true;
+                  this.muro=false;
+                } 
+                if (this.ruta.url == '/muro') {
+                  this.muro=true;
+                  this.perfil=false;
+                } 
+              }
+          }
+    );
+  }
+
+  async navegar(ruta){
+    if(ruta==1){
+      this.ruta.navigateByUrl('/muro')
+    }
+    if(ruta==3){
+      this.ruta.navigateByUrl('/perfil')
+    }
   }
 
 }
