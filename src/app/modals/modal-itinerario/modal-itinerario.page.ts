21d69482903df60e19785b958efb90898d11361d
Julio Bueso <julio.ezequiel@gmail.com>
2019-09-20 15:51:37 -0600

    modales

--- src/app/modals/modal-itinerario/modal-itinerario.page.ts
+++ src/app/modals/modal-itinerario/modal-itinerario.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Input,ViewChild } from '@angular/core';
 import { NavParams, ModalController } from '@ionic/angular';
 
 @Component({
@@ -10,11 +10,49 @@ export class ModalItinerarioPage implements OnInit {
 
   constructor(public modalController:ModalController) { }
 
+   a:any='';
+
+  public anArray:any=[];
+  a:[] = [{"nombre":"1"}]
+
+  contadorDescripcion:any=0;
+  puntos:[]=[
+    {
+      variable:this.contadorDescripcion,
+      nombre:'',
+      tipo:'',
+      gasto:''
+    }
+  ]
   ngOnInit() {
+
   }
 
   cerrar() {
     this.modalController.dismiss();
   }
+
+  agregarPunto(){
+    this.contadorDescripcion=this.contadorDescripcion+1;
+    this.puntos.push({
+      descripcion:this.contadorDescripcion,
+      nombre:'a',
+      tipo:'a',
+      gasto:'a'
+    })
+    
+    console.log(this.puntos);
+    
+  }
+
+  saluda(){
+    // console.log(index,valor,valor2,valor3);   
+    console.log(this.puntos);
+    
+  }
+
+
+
+
 
 }
